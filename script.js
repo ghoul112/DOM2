a542bea76007fcda808027dd9ac17fc833261988
ghoul112 <162143145+ghoul112@users.noreply.github.com>
2024-04-03 14:30:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var colorr=document.getElementById("color");
+console.log(colorr)
+  var  a=document.getElementById('box');
+   var b=document.getElementById('btn');
+colorr.addEventListener("input", function(){
+    a.style.backgroundColor=colorr.value;}
+
+)
